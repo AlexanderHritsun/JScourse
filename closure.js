dc8b48276a9100578f90ca6c7dbb72455f0b254d
Alexander Hritsun <47444468+AlexanderHritsun@users.noreply.github.com>
2020-11-01 21:33:38 +0200

    closure adding

---
+++ closure.js
@@ -0,0 +1,13 @@
+// Closure - in simple words, it's a function which returns inside another function:
+
+const urlGenerator = (domain) => {
+    return (url) => {
+        return `https://${url}.${domain}`;
+    }
+}
+
+const comUrl = urlGenerator('com'); // now comUrl contains the function which was returned by urlGenerator
+const uaUrl = urlGenerator('ua');
+
+console.info(comUrl('google'));
+console.info(uaUrl('dia'));
