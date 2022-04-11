56095ed39a254f39d82abbad61051825f35e9740
Alexander Hritsun <47444468+AlexanderHritsun@users.noreply.github.com>
2022-04-11 17:43:23 +0300

    Update test.js

--- test.js
+++ test.js
@@ -1,2 +1,10 @@
 const test = Object.assign( { a: { a1: 10 }, b1: 100 }, { a: { a2: 20 }, b2: 200 } )
 console.log(test);
+
+const arr = [1, 2, 3]
+const arr2 = arr.reduce( (element) => {
+    const obj = {};
+    obj[element] = element
+} )
+
+console.log('Reducer', arr2);
